fbd1a9c99a6481f3b34f7c279851d66cd7e64f54
Lakshay Kapoor <lakshaykapoor08@gmail.com>
2026-07-21 22:44:48 +0530

    fix(build): export postcss config to fix turbopack build

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,7 @@
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
 };
+
+export default config;
